31da65bda0a82b47c532aecc858a423e69710c69
gabrielcunha91 <128188960+gabrielcunha91@users.noreply.github.com>
2024-10-21 16:35:34 -0300

    Update queries.py

--- utils/queries.py
+++ utils/queries.py
@@ -16,59 +16,85 @@ ORDER BY vfe.`Data`, vfe.Casa
 """
 
 GET_FATURAM_FISCAL = """
-SELECT
-tp.ID as 'tp_ID',
-tf.ID as 'tf_ID',
-tf.DATA_SHOW as 'Data_Show',
-DATE_FORMAT(tf.DATA_SHOW, '%m/%Y') as 'Mes_Ano',
-tc.NAME as 'Casa',
-tc.ID as 'Casa_ID',
-tc.NOTA_FISCAL as 'Casa_Exige_NF',
-tgdc.GRUPO_CLIENTES as 'Grupo_Cliente',
-ta.NOME as 'Artista',
-ta.ID as 'Artista_ID',
-REGEXP_REPLACE(tab.NUMERO_DOCUMENTO, '[^a-zA-Z0-9]', '') as 'Documento_Artista',
-tpf2.ID as 'Boleto_ID',
-DATE(tpf2.DATA_VENCIMENTO) as 'Data_Venc_Boleto',
-tpf2.STATUS as 'Status_Boleto',
-DATE(tpf2.DATA_COMPENSACAO) as 'Data_Pgto_Boleto',
-DATE(tp.DATA_PAGAMENTO) as 'Data_Pgto_Artista',
-tp.VALOR_BRUTO as 'Valor_Bruto',
-tp.VALOR_LIQUIDO as 'Valor_Liquido',
-tf.FATURAM_COMISSAO_ARTISTA as 'Faturam_Comissao_Artista',
-tf.FATURAM_ADIANT_ARTISTA as 'Faturam_Adiant_Artista',
-tf.FATURAM_ADIANT_CONTRATANTE as 'Faturam_Adiant_Contratante',
-tf.FATURAM_SAAS_PERCENTUAL as 'Faturam_Saas_Percentual',
-tf.FATURAM_SAAS_MENSALIDADE as 'Faturam_Saas_Mensalidade',
-tf.FATURAM_CURADORIA as 'Faturam_Curadoria',
-tf.NF_FATURAM_PELO_ARTISTA as 'NF_Pelo_Artista',
-tf.NF_FATURAM_CONTRA_ARTISTA as 'NF_Contra_Artista',
-tf.NF_FATURAM_CONTRA_CONTRATANTE as 'NF_Contra_Contratante',
-(tf.FATURAM_COMISSAO_ARTISTA
- + tf.FATURAM_ADIANT_ARTISTA
- + tf.FATURAM_ADIANT_CONTRATANTE
- + tf.FATURAM_SAAS_PERCENTUAL
- + tf.FATURAM_SAAS_MENSALIDADE
- + tf.FATURAM_CURADORIA) as 'Faturamento_Total',
-tf.DATA_PROCESSADO_NOTA as 'Data_Processam_NF',
-SUBSTRING_INDEX(SUBSTRING_INDEX(tf.NOTA_URL_PDF_PREFEITURA, 'nf=', -1), '&verificacao', 1) AS 'Numero_NF_Eshows',
-tf.NOTA_URL_PDF_PREFEITURA as 'Link_NF_Eshows',
-tf.NOTA_ERRO as 'Erro_NF',
-tf.NOTA_EMAIL_ERRO as 'NOTA_EMAIL_ERRO',
-tnf.NUMERO_NOTA_FISCAL as 'Numero_NF_Artista',
-ef.FILENAME as 'Link_NF_Artista'
+WITH propostas_faturamento AS (
+    SELECT
+        tp.ID AS proposta_ID,
+        tf.ID AS fatura_ID,
+        MAX(tpf2.DATA_VENCIMENTO) AS vencimento_ultima_parcela,
+        COUNT(tpf2.ID) AS numero_parcelas,
+        COUNT(CASE WHEN tpf2.STATUS = 'Received' THEN tpf2.ID END) AS numero_parcelas_pagas,
+        CASE
+            WHEN COUNT(CASE WHEN tpf2.STATUS = 'Received' THEN tpf2.ID END) = COUNT(tpf2.ID)
+                THEN 'QUITADO'
+            WHEN COUNT(CASE WHEN tpf2.STATUS = 'Received' THEN tpf2.ID END) = 0 AND MAX(tpf2.DATA_VENCIMENTO) > CURDATE() 
+                THEN 'PENDENTE'
+            WHEN COUNT(CASE WHEN tpf2.STATUS = 'Received' THEN tpf2.ID END) < COUNT(tpf2.ID) AND MAX(tpf2.DATA_VENCIMENTO) < CURDATE() 
+                THEN 'ATRASADO'
+            ELSE 'PARCIAL'
+        END AS 'Status_Recebimento'
+    FROM T_PROPOSTAS tp
+    LEFT JOIN T_PROPOSTAS_FATURA tpf ON (tp.ID = tpf.FK_PROPOSTA)
+    LEFT JOIN T_FATURAS tf ON (tpf.FK_FATURA = tf.ID)
+    LEFT JOIN T_PARCELAS_FATURA tpf2 ON (tf.ID = tpf2.FK_FATURA)
+    WHERE tpf2.CANCELADO = 0 
+    GROUP BY tp.ID
+)
+SELECT 
+    tp.ID AS 'tp_ID',
+    tf.ID AS 'tf_ID',
+    tf.DATA_SHOW AS 'Data_Show',
+    DATE_FORMAT(tf.DATA_SHOW, '%m/%Y') AS 'Mes_Ano',
+    tc.NAME AS 'Casa',
+    tc.ID AS 'Casa_ID',
+    tc.NOTA_FISCAL AS 'Casa_Exige_NF',
+    tgdc.GRUPO_CLIENTES AS 'Grupo_Cliente',
+    ta.NOME AS 'Artista',
+    ta.ID AS 'Artista_ID',
+    REGEXP_REPLACE(tab.NUMERO_DOCUMENTO, '[^a-zA-Z0-9]', '') AS 'Documento_Artista',
+    pf.fatura_ID AS 'Fatura_ID',
+    pf.vencimento_ultima_parcela AS 'Vencimento_Ultima_Parcela',
+    pf.numero_parcelas AS 'Numero_Parcelas',
+    pf.numero_parcelas_pagas AS 'Numero_Parcelas_Pagas',
+    pf.Status_Recebimento AS 'Status_Recebimento',
+    MAX(DATE(tp.DATA_PAGAMENTO)) AS 'Data_Pgto_Artista',
+    MAX(tp.VALOR_BRUTO) AS 'Valor_Bruto',
+    MAX(tp.VALOR_LIQUIDO) AS 'Valor_Liquido',
+    MAX(tf.FATURAM_COMISSAO_ARTISTA) AS 'Faturam_Comissao_Artista',
+    MAX(tf.FATURAM_ADIANT_ARTISTA) AS 'Faturam_Adiant_Artista',
+    MAX(tf.FATURAM_ADIANT_CONTRATANTE) AS 'Faturam_Adiant_Contratante',
+    MAX(tf.FATURAM_SAAS_PERCENTUAL) AS 'Faturam_Saas_Percentual',
+    MAX(tf.FATURAM_SAAS_MENSALIDADE) AS 'Faturam_Saas_Mensalidade',
+    MAX(tf.FATURAM_CURADORIA) AS 'Faturam_Curadoria',
+    MAX(tf.NF_FATURAM_PELO_ARTISTA) AS 'NF_Pelo_Artista',
+    MAX(tf.NF_FATURAM_CONTRA_ARTISTA) AS 'NF_Contra_Artista',
+    MAX(tf.NF_FATURAM_CONTRA_CONTRATANTE) AS 'NF_Contra_Contratante',
+    (MAX(tf.FATURAM_COMISSAO_ARTISTA)
+     + MAX(tf.FATURAM_ADIANT_ARTISTA)
+     + MAX(tf.FATURAM_ADIANT_CONTRATANTE)
+     + MAX(tf.FATURAM_SAAS_PERCENTUAL)
+     + MAX(tf.FATURAM_SAAS_MENSALIDADE)
+     + MAX(tf.FATURAM_CURADORIA)) AS 'Faturamento_Total',
+    MAX(tf.DATA_PROCESSADO_NOTA) AS 'Data_Processam_NF',
+    MAX(SUBSTRING_INDEX(SUBSTRING_INDEX(tf.NOTA_URL_PDF_PREFEITURA, 'nf=', -1), '&verificacao', 1)) AS 'Numero_NF_Eshows',
+    MAX(tf.NOTA_URL_PDF_PREFEITURA) AS 'Link_NF_Eshows',
+    MAX(tf.NOTA_ERRO) AS 'Erro_NF',
+    MAX(tf.NOTA_EMAIL_ERRO) AS 'NOTA_EMAIL_ERRO',
+    MAX(tnf.NUMERO_NOTA_FISCAL) AS 'Numero_NF_Artista',
+    MAX(ef.FILENAME) AS 'Link_NF_Artista'
 FROM T_FATURAMENTO tf 
 INNER JOIN T_PROPOSTAS tp ON (tf.FK_PROPOSTA = tp.ID)
 INNER JOIN T_ATRACOES ta ON (tp.FK_CONTRATADO = ta.ID)
 INNER JOIN T_COMPANIES tc ON (tp.FK_CONTRANTE = tc.ID)
 LEFT JOIN T_ATRACAO_BANCOS tab ON (tp.FK_ATRACAO_BANCO = tab.ID)
 LEFT JOIN T_GRUPOS_DE_CLIENTES tgdc ON (tc.FK_GRUPO = tgdc.ID)
-JOIN T_PROPOSTAS_FATURA tpf ON (tp.ID = tpf.FK_PROPOSTA)
-JOIN T_PARCELAS_FATURA tpf2 ON (tpf.FK_FATURA = tpf2.FK_FATURA)
+LEFT JOIN propostas_faturamento pf ON (tp.ID = pf.proposta_ID)
 LEFT JOIN T_NOTAS_FISCAIS tnf ON (tp.FK_NOTA_FISCAL = tnf.ID AND tnf.FK_STATUS_NF = 101)
 LEFT JOIN EPM_FILES ef ON (ef.TABLE_NAME = "T_NOTAS_FISCAIS" AND ef.TABLE_ID = tnf.ID)
-WHERE tf.DATA_SHOW > '2024-01-01 00:00:00'
-ORDER BY tf.ID DESC
+WHERE tp.FK_STATUS_PROPOSTA NOT IN (102)
+AND tc.ID NOT IN (102, 196, 633)
+AND tf.DATA_SHOW > '2024-01-01 00:00:00'
+GROUP BY tp.ID
+ORDER BY tf.ID
 """
 
 GET_CUSTOS_INTERNOS = """
